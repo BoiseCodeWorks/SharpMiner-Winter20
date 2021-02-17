4568f78b6305ffc78aaafab3ae3039e9b4a1cf0b
MickShannahan <beasticorn@gmail.com>
2021-02-17 10:25:12 -0700

    added notes

--- Program.cs
+++ Program.cs
@@ -13,6 +13,8 @@ namespace SpaceGame
     }
   }
 
+
+  // object class used to build out our upgrades
   class Upgrade
   {
     public string Name { get; private set; }
@@ -30,6 +32,8 @@ namespace SpaceGame
       GenValue = genValue;
     }
   }
+
+  //   Game class that wil hold the data of the game.
   class SpaceGame
   {
     public bool Running { get; set; }
@@ -40,6 +44,7 @@ namespace SpaceGame
     public List<Upgrade> AutoUpgrades { get; private set; }
     public Dictionary<string, int> Stats { get; set; }
 
+    // The constructor is basically the new game setup, it creates an instance of all the Lists and Dictionaries needed, and creates the shop items, and gives the player 1 pick axe to start.
     public SpaceGame()
     {
       Running = true;
@@ -59,8 +64,9 @@ namespace SpaceGame
       StartTimer();
       while (Running)
       {
-        var input = GetPlayerInput();
-        switch (input.Key.ToString().ToLower())
+        //   Get player input captures the players key press, and converts it from a "ConsoleKeyinfo" into a string we can check.
+        string input = GetPlayerInput().Key.ToString().ToLower();
+        switch (input)
         {
           case "spacebar":
             Mine();
@@ -68,15 +74,21 @@ namespace SpaceGame
           case "s":
             GoToShop();
             break;
+          case "escape":
+            Running = false;
+            break;
         }
       }
     }
+
+    // Draws the screen (the moon), and then waits to read the user's key press.
     public ConsoleKeyInfo GetPlayerInput()
     {
       DrawGameScreen();
       return Console.ReadKey();
     }
 
+    // iterates over click mining objects and adds their Quantity * generation Value to you Cheese.
     public void Mine()
     {
       ClickUpgrades.ForEach(miner =>
@@ -86,28 +98,34 @@ namespace SpaceGame
       Console.WriteLine($"Cheese:{Cheese}");
     }
 
+    // iterates over automine objects and adds their Quantity * generation Value to you Cheese. Timer callbacks need to take in an object as a parameter.
     public void AutoMine(object o)
     {
       AutoUpgrades.ForEach(miner =>
    {
      Cheese += (miner.Quantity * miner.GenValue) * 5;
    });
+      //  keeps our auto interval from interupting our user while in another game screen
       if (inShop == false)
       {
         DrawGameScreen();
       }
     }
+    // starts the interval to automatically add our cheese items
     public void StartTimer()
     {
       Timer timer = new Timer(AutoMine, null, 0, 5000);
     }
 
+
+    // opens the shop Menu
     public void GoToShop()
     {
       inShop = true;
       Console.Clear();
       Console.WriteLine("Welcome to the eht shopp'd, what would you like to buy?");
       string message = "";
+      //   iterates over the items available in the shop and displays them with a number corresponding to the purchase we want to make.
       for (int i = 0; i < Shop.Count; i++)
       {
         Upgrade item = Shop[i];
@@ -115,12 +133,14 @@ namespace SpaceGame
       }
       Console.WriteLine(message);
       string choice = Console.ReadLine();
+      //   Checks to see if choice of player is a valid selection.
       if (int.TryParse(choice, out int selection) && selection > 0 && selection <= Shop.Count)
       {
         BuyUpgrade(selection - 1);
       }
     }
 
+    // Purchases an item from the shop menu
     public void BuyUpgrade(int shopIndex)
     {
       Upgrade item = Shop[shopIndex];
@@ -130,6 +150,7 @@ namespace SpaceGame
         item.Price += item.Price;
         if (item.Type == "click")
         {
+          // This checks to see if we already have purchased this upgrade, if we have not, we will add it to our upgrades, if we do it will increment the quantity of the upgrade we have.
           int index = ClickUpgrades.FindIndex(i => i.Name == item.Name);
           if (index == -1)
           {
@@ -141,6 +162,7 @@ namespace SpaceGame
         }
         else
         {
+          // This checks to see if we already have purchased this upgrade, if we have not, we will add it to our upgrades, if we do it will increment the quantity of the upgrade we have.
           int index = AutoUpgrades.FindIndex(i => i.Name == item.Name);
           if (index == -1)
           {
@@ -157,6 +179,7 @@ namespace SpaceGame
       {
         Console.WriteLine($"you don't heav enough cheez to by {item.Name}... go away.");
       }
+      //   Using a Readkey so the user has a moment to read the message before they continue.
       Console.WriteLine("press any key to continue");
       Console.ReadKey();
       inShop = false;
@@ -167,6 +190,8 @@ namespace SpaceGame
 
 
 
+
+    // draws the games "screen"  keeping the amount of "Console.WritLines()" down to help with the responsiveness of the progarm.
     public void DrawGameScreen()
     {
       Console.Clear();
